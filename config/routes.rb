7b84f0b9f0cc0940db7b5f2f7abacbbf2351315d
Gareth Cary <gareth@caryfamily.co.uk>
2020-05-17 23:47:06 +0100

    style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :contacts, only: [:new, :create]
+  resources :contacts, only: [:new, :create]
 
-root to: 'pages#home'
-get 'create', to: 'contacts#create'
+  root to: 'pages#home'
+  get 'create', to: 'contacts#create'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'portfolio', to: 'pages#portfolio'
