c8bc02b16186ceaa78b4c40a8672a305d6ff7afa
Gareth Cary <gareth@caryfamily.co.uk>
2020-05-17 21:44:06 +0100

    style contact form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 resources :contacts, only: [:new, :create]
 
 root to: 'pages#home'
+get 'create', to: 'contacts#create'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'portfolio', to: 'pages#portfolio'
